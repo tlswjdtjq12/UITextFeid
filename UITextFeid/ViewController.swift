8c6ec7107348a5019141579ebcbfed9ec908f4f7
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-03-22 15:34:12 +0900

    BorderStyle

--- UITextFeid/ViewController.swift
+++ UITextFeid/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl01: UILabel!
+    
+    @IBOutlet weak var txt01: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        txt01.placeholder = "입력을 하세요"
+        txt01.clearButtonMode = UITextFieldViewMode.whileEditing
+        txt01.borderStyle = UITextBorderStyle.line
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btn01(_ sender: Any) {
+        lbl01.text = "Hello " + txt01.text!
+        txt01.text = ""
+        txt01.resignFirstResponder()
+    }
+    
 }
-
