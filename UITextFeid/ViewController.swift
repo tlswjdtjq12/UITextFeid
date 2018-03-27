3d398db1a8dccf625e7f358f533bfb32bf2761ed
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-03-27 15:18:09 +0900

    Delegate

--- UITextFeid/ViewController.swift
+++ UITextFeid/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var lbl01: UILabel!
     
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        // UITextFieldDelagte 객체와 viewController 객체와 연결
+        txt01.delegate = self
         txt01.placeholder = "입력을 하세요"
         txt01.clearButtonMode = UITextFieldViewMode.whileEditing
         txt01.borderStyle = UITextBorderStyle.line
@@ -37,5 +39,19 @@ class ViewController: UIViewController {
     //backgroung의 view를 누르면 키패드가 내려간다
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         txt01.resignFirstResponder()
+    }
+    
+    //UITextFiedlDelgate 메소드 호출
+    //Delegate 함수 : 특정시점에서 호출된 함수
+    //called when 'return' key pressed. return No to ignore.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.backgroundColor = UIColor.yellow
+        txt01.resignFirstResponder()
+        return true
+    }
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        view.backgroundColor = UIColor.green
+        txt01.resignFirstResponder()
+        return true
     }
 }
